fa77cbfaab28860bb177d26617fb8756f85ded24
Hesed Anu Afolami <hesedanu@gmail.com>
2025-11-22 19:55:12 +0000

    the webpage is not rightly laced on top review that

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -11,10 +11,10 @@ export default function MainLayout({
     <SidebarProvider>
       <div className="min-h-screen w-full">
         <AppSidebar />
-        <SidebarInset>
+        <div className="flex flex-col">
             <AppHeader />
             <main className="p-4 sm:p-6 lg:p-8">{children}</main>
-        </SidebarInset>
+        </div>
       </div>
     </SidebarProvider>
   );
